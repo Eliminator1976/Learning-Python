d3cfd3bf8d5efbc292d86b6794ed95081154145b
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-05-30 20:39:33 +0530

    revised code and completed

--- week2/plates.py
+++ week2/plates.py
@@ -1,45 +1,43 @@
 def main():
-    plate = input("Plate: ")
+    plate = input("Plate: ").upper().strip()
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
-        
+nope = ' .,!&-_'
+nope = list(nope)
+number = '0123456789'
+number = list(number)
+
 def is_valid(s):
     p = list(s)
-    if len(list(p)) <2 or len(list(p)) >6:
+    if len(p) <2 or len(p)>6:
         return False
     for i in s[0:2]:
-        if i.isalpha == True:
-            return True
-    elif numinchar(s) == False:
-        return True
-    elif nspclchr(s) == True:
-        return True
-    
-def numinchar(s):
-    s = list(s)
-    nums = '0123456789'
-    p = list(nums)
-    
-            
-            
-def nspclchr(s):
-    p = list(s)
-    spcl = ['!','@','#','$','%','&','*']
-    for i in p:
-        if set(i).isdisjoint(spcl)
-        return True
+        if i.isalpha() == False:
+            return False
+    for i in nope:
+        if i in p:
+            return False
+    for num in p:
+        if num.isdigit():
+            if num == '0':
+                return False
+            break
+    for i in number:
+        if i in p:
+            n = p.index(i)
+            break
+    for i in s[n:]:
+        if i.isdigit() == False:
+            return False
     else:
-        return False
-    
-# checks for no special characters
-    
-    
+        return True
 
 main()
 
 #start with atleast two letters
 #maximum of 6characters letters or numbers and minimum 2
-#numbers should not be between letters and first number not 0
+#numbers should not be between letters
+# and first number not 0
 #no periods spaces punctuation marks allowed

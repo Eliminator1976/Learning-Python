00f3bfff23bb87e41781c07ea82f57ecd286ea64
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-06-03 10:15:14 +0530

    Delete Week-2/week2-temp directory

--- Week-2/week2-temp/camel.py
+++
@@ -1,8 +0,0 @@
-reply = input("camelCase: ")
-print("snake_case: ", end="")
-for char in reply:
-    if char == char.lower():
-        print(char, end="")
-    else:
-        print("_" + char.lower(), end="")
-print()  

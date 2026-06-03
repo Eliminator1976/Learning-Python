a5eac48a124b80dbf141b9af5d321506c6fa980b
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-06-03 17:19:48 +0530

    Problem 4 from ps_3 solved

---
+++ Week-3/Outdated.py
@@ -0,0 +1,66 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+def main():
+    while True:
+        date = input("Date: ").title()
+        if "/" in list(date):
+            # format = MM/DD/YYYY
+            date_analog = date_analog_handler(date)
+            print(date_analog)
+            break
+        elif "," in list(date):
+            # format = month day, year
+            date_easy = date_easy_handler(date)
+            print(date_easy)
+            break
+        else:
+            print("unsupported format error")
+            break
+            
+def date_analog_handler(date):
+    date = date.split(sep="/")
+    #becomes a list as ['MM','DD','YYYY']
+    if not date[0].startswith("0") or date[0].startswith("1"):
+        date[0] = date[0].replace(date[0],"0"+date[0])        
+    if not date[1].startswith("0") or date[1].startswith("1") or date[1].startswith("2") or date[1].startswith("3"):
+        date[1] = date[1].replace(date[1],"0"+date[1])
+    return f"{date[2]}-{date[0]}-{date[1]}"
+
+# temp paste (# format = month day, year)
+
+def date_easy_handler(date):
+    date = date.replace(",", "").split()
+    # becomes a list as ['month', 'day', 'year']
+    for month in months:
+        if month == date[0]:
+            x = int(months.index(month)) + 1
+            date[0] = str(x)
+        else:
+            break
+    # month became a number without 0 formatting
+    if not date[0].startswith("0") or date[0].startswith("1"):
+        date[0] = date[0].replace(date[0],"0"+date[0])        
+    if not date[1].startswith("0") or date[1].startswith("1") or date[1].startswith("2") or date[1].startswith("3"):
+        date[1] = date[1].replace(date[1],"0"+date[1])
+    # month and date get 0 formatted
+    return f"{date[2]}-{date[0]}-{date[1]}"
+    
+main()        
+    #YYYY-MM-DD required format
+    
+    
+    
+    
+    

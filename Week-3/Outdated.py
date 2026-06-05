2fb02c8e19ffb754820c9e04da726e7e159c1e55
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-06-05 09:08:21 +0530

    fixed remaining bugs 🧠

--- Week-3/Outdated.py
+++ Week-3/Outdated.py
@@ -35,11 +35,11 @@ def main():
 def date_analog_handler(date):
     date = date.split(sep="/")
     # becomes a list as ['MM','DD','YYYY']
-    
-    if int(date[0]) > 12 and int(date[1]) > 31:
+
+    if int(date[0]) > 12 or int(date[1]) > 31:
         return f"unpossible date 😭"
-    
-    if not date[0].startswith("0") and  not date[0].startswith("1"):
+
+    if not date[0].startswith("0") and not date[0].startswith("1"):
         date[0] = date[0].replace(date[0], "0" + date[0])
     if (
         not date[1].startswith("0")
@@ -49,7 +49,6 @@ def date_analog_handler(date):
     ):
         date[1] = date[1].replace(date[1], "0" + date[1])
     return f"{date[2]}-{date[0]}-{date[1]}"
-    
 
 
 # temp paste (# format = month day, year)
@@ -60,15 +59,19 @@ def date_easy_handler(date):
     # becomes a list as ['month', 'day', 'year']
     try:
         x = months.index(date[0]) + 1
-        date[0] = str(x)
+        if x < 10:
+            date[0] = f"0{str(x)}"
+        else:
+            date[0] = str(x)
+
     except ValueError:
         return f"unknown month"
-        
+
     # month became a number without 0 formatting
     if int(date[1]) > 31:
         return f"unpossible date 😭"
-    
-    if date[0].startswith("0") and not date[0].startswith("1"):
+
+    if not date[0].startswith("0") and not date[0].startswith("1"):
         date[0] = date[0].replace(date[0], "0" + date[0])
     if (
         not date[1].startswith("0")

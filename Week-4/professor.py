7c56a9f1f17b8d04b13d337fc2a4b5d71ac6f468
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-06-08 17:52:44 +0530

    Reformatting with {black plugin}

--- Week-4/professor.py
+++ Week-4/professor.py
@@ -3,21 +3,21 @@ import random
 
 def main():
     level = get_level()
-    
+
     while True:
         operator = input("Opearator: ")
-        operators = ['-','+','*','/']
+        operators = ["-", "+", "*", "/"]
         if operator in operators:
             break
         else:
             continue
     j = 0
     k = 0
-    while k<10:
+    while k < 10:
         x = generate_integer(level)
         y = generate_integer(level)
-        calc_data = [x,operator,y]
-        
+        calc_data = [x, operator, y]
+
         i = 0
         real_answer = operator_handler(*calc_data)
         while i < 3:
@@ -33,37 +33,38 @@ def main():
                     print(f"{x} {operator} {y} = {real_answer}")
                 else:
                     continue
-    print("Great Game! \n"+"Your score is "+ str(j*10))
-         
+    print("Great Game! \n" + "Your score is " + str(j * 10))
+
+
 def operator_handler(x, operator, y):
     match operator:
-        case '+':
-            return x+y
-        case '-':
-            return x-y
-        case '*':
-            return x*y
-        case '/':
-            return x/y
+        case "+":
+            return x + y
+        case "-":
+            return x - y
+        case "*":
+            return x * y
+        case "/":
+            return x / y
+
 
 def get_level():
     while True:
         try:
             n = int(input("Level: "))
-            allowed_levels = [1,2,3]
+            allowed_levels = [1, 2, 3]
             if n in allowed_levels:
                 break
         except ValueError:
             continue
-            
+
     return n
-    
 
 
 def generate_integer(level):
-    start = 1 if level == 1 else 10**(level-1)
+    start = 1 if level == 1 else 10 ** (level - 1)
     end = (10**level) - 1
-    return random.randint(start,end)
+    return random.randint(start, end)
 
 
 if __name__ == "__main__":

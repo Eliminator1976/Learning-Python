b4b334b79f3d075ce705ae98fffdbefa4914e603
LazyCupCake <258856984+Eliminator1976@users.noreply.github.com>
2026-06-11 00:23:18 +0530

    add more assert statements

--- Week-5/test_twttr.py
+++ Week-5/test_twttr.py
@@ -3,3 +3,6 @@ from twttr import shorten
 def test_shorten():
     assert shorten("hi this is my twitter handle") == "h ths s my twttr hndl"
     assert shorten("i am feeling lucky") == " m flng lcky"
+    assert shorten("clouds are amazing today") == "clds r mzng tdy"
+    assert shorten("this is a wonderful day") == "ths s  wndrfl dy"
+    assert shorten("storm my brain with ideas") == "strm my brn wth ds"
